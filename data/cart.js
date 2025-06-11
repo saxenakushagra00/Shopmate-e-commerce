099f6a5b6f8215544412a145abb6a00dfb6c4523
kushagra <saxenakushagra433@gmail.com>
2025-06-11 19:58:15 +0530

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,19 @@
-export let cart=[{
+export let cart=
+
+JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart=[{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2
 },{
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
 }];
+}
 
-
+function saavetostorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 export function addtocart(productId){
 let matchingitem;
 cart.forEach((cartitem)=>{
@@ -23,6 +30,7 @@ cart.push({
   quantity:1
 });
 }
+saavetostorage();
 }
 
 export function removefromcart(productId){
@@ -35,4 +43,5 @@ if(cartitem.productId!=productId){
 });
 
 cart=newcart;
+saavetostorage();
 }
