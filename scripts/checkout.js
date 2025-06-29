19a24b0f6d35bffdfa8b26fe6d58a9b4fde97e78
kushagra <saxenakushagra433@gmail.com>
2025-06-29 17:32:37 +0530

    add backend practise file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 //import'../data/cart-class.js';
 import { renderordersummary } from './checkout/ordersummary.js';
 import { renderpaymentsummary } from './checkout/paymentsummary.js';
+import '../data/backend-practice.js';
 renderordersummary();
 renderpaymentsummary();
